587bfc916380353e7cf106f6c5e1e9461afd5575
Kedia Corporation <corporate@kedia.us>
2014-12-17 17:40:29 -0500

    Maintenance

--- app/mailers/deposit_mailer.rb
+++ app/mailers/deposit_mailer.rb
@@ -2,7 +2,7 @@ class DepositMailer < BaseMailer
 
   def accepted(deposit_id)
     @deposit = Deposit.find deposit_id
-    mail to: @deposit.member.email
+    #mail to: @deposit.member.email
   end
 
 end
